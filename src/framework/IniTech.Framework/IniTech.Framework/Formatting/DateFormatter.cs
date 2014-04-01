9df7df962fce4b3c0e05c173ed634eecef98cb7d
Maarten Balliauw <maarten@maartenballiauw.be>
2014-04-01 13:03:30 +0200

    Initial checkin of framework

---
+++ src/framework/IniTech.Framework/IniTech.Framework/Formatting/DateFormatter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IniTech.Framework.Formatting
+{
+    public static class DateFormatter
+    {
+        public static string Format(DateTime value)
+        {
+            return value.ToShortDateString();
+        }
+    }
+}
---
+++ src/framework/IniTech.Framework/IniTech.Framework/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("IniTech.Framework")]
+[assembly: AssemblyDescription("IniTech Framework contains shared components for many projects developed by IniTech. Didn't you get that memo?")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("IniTech")]
+[assembly: AssemblyProduct("IniTech.Framework")]
+[assembly: AssemblyCopyright("Copyright © IniTech 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c4bd1374-0b8d-468c-bb9a-f15ed3da06d4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
